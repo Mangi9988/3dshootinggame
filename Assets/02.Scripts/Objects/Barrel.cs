1ae959428682d0df578d2fddec4b48a95c7c2c61
Mangi9988 <bibijh9988@gmail.com>
2025-04-25 03:48:33 +0900

    정신나갈거같아
    외않되?????????? 그냥 자야겠다

--- Assets/02.Scripts/Objects/Barrel.cs
+++ Assets/02.Scripts/Objects/Barrel.cs
@@ -1,25 +1,86 @@
+using System.Collections;
 using UnityEngine;
 
 // 폭발 가능 대상은 IDamageable 구현
 public class Barrel : MonoBehaviour, IDamageable
 {
-    // 미리 프리팹으로 만들어 둔 Explosion 컴포넌트
-    [SerializeField] private Explosion _barrelExplosionPrefab;
-
-    // 데미지를 받으면 곧바로 폭발
+    [Header("체력")]
+    [SerializeField] private int _maxHealth = 100;
+    private int _currentHealth;
+    private bool _isDead;
+    
+    [Header("이펙트")]
+    public Explosion ExplosionPrefab;
+    
+    [Header("오브젝트 변경")]
+    [SerializeField] private GameObject _beforeObject;
+    [SerializeField] private GameObject _afterObject;
+    [SerializeField] private float _destroyDelayTime;
+    
+    [Header("날리기")]
+    [SerializeField] private float _pieceExplosionForce = 2f;
+    [SerializeField] private float _pieceExplosionTorque = 10f;
+    public float MinForceRandomRange = -0.5f;
+    public float MaxForceRandomRange = 0.5f;
+    public float MinTorqueRandomRange = -0.5f;
+    public float MaxTorqueRandomRange = 0.5f;
+    
+    private Rigidbody _rigidbody;
+    
+    private void Start()
+    {
+        _currentHealth = _maxHealth;
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+    
     public void TakeDamage(Damage damage)
     {
-        // 1) 폭발 위치에 Explosion 인스턴스 띄우고
+        if (_isDead)
+            return;
+
+        _currentHealth -= damage.Value;
+        if (_currentHealth <= 0)
+            Die();
+    }
+    
+    private void Die()
+    {
+        _isDead = true;
+
+        // 1) 폭발 이펙트 생성 및 폭발 로직 호출
+
         Explosion explosion = Instantiate(
-            _barrelExplosionPrefab,
+            ExplosionPrefab,
             transform.position,
             Quaternion.identity
         );
-
-        // 2) 즉시 폭발 로직 실행
         explosion.Explode();
+        
 
-        // 3) Barrel 오브젝트는 제거
+        // 2) 외형 전환
+        _beforeObject.SetActive(false);
+        _afterObject.SetActive(true);
+
+        // 3) 파편 물리 넉백
+        Vector3 randDirection = new Vector3
+            (Random.Range(MinForceRandomRange, MaxForceRandomRange), 
+            1f,
+            Random.Range(MinForceRandomRange, MaxForceRandomRange)).normalized;
+        
+        _rigidbody.AddForce(randDirection * _pieceExplosionForce, ForceMode.Impulse);
+
+        Vector3 randTorque = new Vector3
+            (1f, Random.Range(MinTorqueRandomRange, MaxTorqueRandomRange), 
+            Random.Range(MinTorqueRandomRange, MaxTorqueRandomRange)).normalized * _pieceExplosionTorque;
+        _rigidbody.AddTorque(randTorque, ForceMode.Impulse);
+
+        // 4) 지정 시간 후 오브젝트 제거
+        StartCoroutine(DestroyAfterDelay());
+    }
+
+    private IEnumerator DestroyAfterDelay()
+    {
+        yield return new WaitForSeconds(_destroyDelayTime);
         Destroy(gameObject);
     }
 }
